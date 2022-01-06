e0a784e862354332aa8c136abe79dcc66b7fbac6
ram5353 <ramkiran5353@gmail.com>
2022-01-06 18:22:02 +0530

    user login

--- src/main/java/com/userportal/resource/UserResource.java
+++ src/main/java/com/userportal/resource/UserResource.java
@@ -2,13 +2,19 @@ package com.userportal.resource;
 
 
 import com.userportal.domain.User;
+import com.userportal.domain.UserPrincipal;
 import com.userportal.exception.domain.EmailExistException;
 import com.userportal.exception.domain.UserNotFoundException;
 import com.userportal.exception.domain.UsernameExistException;
 import com.userportal.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import static com.userportal.constant.SecurityConstant.*;
+
+import com.userportal.utility.JWTTokenProvider;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
@@ -20,12 +26,33 @@ import static org.springframework.http.HttpStatus.OK;
 @RequestMapping(value = "/user")
 public class UserResource {
 
-    @Autowired
     private UserService userService;
+    private AuthenticationManager authenticationManager;
+    private JWTTokenProvider jwtTokenProvider;
 
-    @GetMapping("/home")
-    public String showUser() throws UserNotFoundException {
-        throw new UserNotFoundException("User not found");
+    public UserResource(UserService userService, AuthenticationManager authenticationManager, JWTTokenProvider jwtTokenProvider) {
+        this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user) {
+        authenticate(user.getUsername(), user.getPassword());
+        User loginUser = userService.findUserByUsername(user.getUsername());
+        UserPrincipal userPrincipal = new UserPrincipal(loginUser);
+        HttpHeaders jwtHeader = getJwtHeader(userPrincipal);
+        return new ResponseEntity<>(loginUser, jwtHeader, OK);
+    }
+
+    private void authenticate(String username, String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+    }
+
+    private HttpHeaders getJwtHeader(UserPrincipal user) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(JWT_TOKEN_HEADER, jwtTokenProvider.generateJwtToken(user));
+        return headers;
     }
 
     @GetMapping("/list")
