1a502df27c82e73049199428ccff58684cf768cc
ram5353 <ramkiran5353@gmail.com>
2022-03-09 15:30:50 -0500

    Add, Update, Delete User API's

--- src/main/java/com/userportal/UserportalApplication.java
+++ src/main/java/com/userportal/UserportalApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import static com.userportal.constant.FileConstant.*;
+
+import java.io.File;
 
 @SpringBootApplication
 public class UserportalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserportalApplication.class, args);
+        new File(USER_FOLDER).mkdirs();
     }
 
     @Bean
